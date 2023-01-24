cfeaa46c3a06ccc0004dee65e9008169fe7579eb
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-24 18:40:43 +0200

    navigate after signin needs fix

--- chat-app/src/components/Chats.js
+++ chat-app/src/components/Chats.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "../firebase";
+
+import { useAuth } from "../contexts/AuthContext";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  const navigate = useNavigate();
+  const { user } = useAuth();
+
+  console.log(user);
+  const handleLogout = async () => {
+    await auth.signOut();
+    navigate("/", { replace: true });
+  };
+  return (
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">TotalChat</div>
+        <div onClick={handleLogout} className="logout-tab">
+          Logout
+        </div>
+      </div>
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectID={process.env.REACT_APP_PROJECT_ID}
+        userName={process.env.REACT_APP_USERNAME}
+        userSecret={process.env.REACT_APP_USER_SECRET}
+      />
+    </div>
+  );
 };
 
 export default Chats;
