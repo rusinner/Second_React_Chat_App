62711af4b33e39821f85d8bcab1f9b2818ce35da
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-24 14:49:08 +0200

    chats page

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Chats from "./components/Chats";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <div style={{ fontFamily: "Avenir" }}>
       <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/chats" element={<Chats />} />
+          </Routes>
+        </AuthProvider>
       </Router>
     </div>
   );
